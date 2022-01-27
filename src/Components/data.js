d6dc5b0b93bdf69b24f6026183023517f7000162
Dawit Hailu Birhane <Davidhailu0@gmail.com>
2022-01-27 03:17:21 -0800

    add data.js file

---
+++ src/Components/data.js
@@ -0,0 +1,6 @@
+export const links = ['About AASTU','Admissions','Academics','University Life','Research'];
+export const aboutAASTULinks = ['Leadership & University Admininstration','News, Publications, and Facts', 'Visitor Information', 'Contact Us']
+export const admissionLinks = ['Undergraduate Admission','Graduate Admissions','Tuition and Fees']
+export const lifeAASTULinks = ['Arts & Culture','Athletics and Recreation','Residential Life']
+export const researchLinks = ['Research at AASTU','Libraries','Museums and Galleries','Centers and Institutes']
+export const academicLinks = ['Undergraduate Study','Graduate & Professional Study','Department & Programs']
